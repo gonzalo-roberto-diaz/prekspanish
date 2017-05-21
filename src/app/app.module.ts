cf97eda8197d6665b28de86a852d1cdf4f6c87ab
gonzalodiaz <gonzalo.diaz@radius.com>
2017-05-20 21:40:21 -0700

    add main components, lay out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { VideosComponent } from './videos/videos.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ResumeComponent,
+    VideosComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
