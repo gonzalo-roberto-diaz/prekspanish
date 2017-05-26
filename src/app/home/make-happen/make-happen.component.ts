0fc1d683cc49b835afd9a16262868e3ec5150186
gonzalodiaz <gonzalo.diaz@radius.com>
2017-05-25 22:59:19 -0700

    make happen section

---
+++ src/app/home/make-happen/make-happen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-make-happen',
+  templateUrl: './make-happen.component.html',
+  styleUrls: ['./make-happen.component.css']
+})
+export class MakeHappenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
